dab10eb7622b49752f7bf1ab0b2b23b788b32277
Patrick Wu <magetron1@gmail.com>
2018-10-27 06:50:57 +0100

    add KTH Challenges 2017 B evenodd

--- KTH-Challenge-2017/evenodd.c
+++ KTH-Challenge-2017/evenodd.c
@@ -1,26 +1,28 @@
 #include "stdio.h"
 #define modulo 1000000007
 
-long long l, r, s, i;
-
 long long f(long long x) {
 	long long i = 0;
 	while (x > 1) {
-		if (x % 2 == 0) x = x / 2;
+		if (x % 2 == 0) x /= 2;
 		else x++;
 		i++;
 	}
 	return i % modulo;
 }
 
-int main () {
-	scanf("%lld %lld", &l, &r);
-	s = 0;
-	
-	for (i = l; i <= r; i++) s = (s + f(i)) % modulo;
-	
-	printf("%lld\n", s);
-
-	return 0;
+long long ans(long long x) {
+	if (x <= 1) return 0;
+	else if (x % 2 == 1) 
+		return (ans(x - 1) + f(x)) % modulo;
+	else if (x % 2 == 0)
+		return (x / 2 + x - 2 + 2 * ans(x / 2)) % modulo;
 }
 
+long long l, r;
+
+int main () {
+	scanf("%lld %lld", &l, &r);
+	printf("%lld", (ans(r) - ans(l - 1) + modulo) % modulo);
+	return 0;
+}
