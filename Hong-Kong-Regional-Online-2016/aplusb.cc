fd736ef768e28488cad4b7fd16283177d2bc2267
Patrick Wu <magetron1@gmail.com>
2018-11-06 08:24:00 +0000

    aplusb finished

--- Hong-Kong-Regional-Online-2016/aplusb.cc
+++ Hong-Kong-Regional-Online-2016/aplusb.cc
@@ -1,75 +1,93 @@
 #include <cstdio>
 #include <cstring>
-#include <complex>
-#define max_num 50005
-#define max_n 200005
-
-typedef std::complex<double> cplx;
+#include <cstdlib>
+#include <cmath>
+#include <algorithm>
+#define max_n 200000
+#define max 50000
 
 const double PI = acos(-1.0);
 
-inline unsigned int intrev(unsigned x) {
-	x = (( x & 0x55555555U) << 1) | (( x & 0xAAAAAAAAU) >> 1);
-	x = (( x & 0x33333333U) << 2) | (( x & 0xCCCCCCCCU) >> 2);
-	x = (( x & 0x0F0F0F0FU) << 4) | (( x & 0xF0F0F0F0U) >> 4);
-	x = (( x & 0x00FF00FFU) << 8) | (( x & 0xFF00FF00U) >> 8);
-	x = (( x & 0x0000FFFFFU) << 16) | (( x & 0xFFFF0000U) >> 16);
-	return x;
+struct complex {
+	double r, i;
+	complex(double real = 0.0, double imag = 0.0) {
+		r = real;
+		i = imag;
+	}
+	complex operator + (const complex &e) {
+		return complex(r + e.r, i + e.i);
+	}
+	complex operator - (const complex &e) {
+		return complex(r - e.r, i - e.i);
+	}
+	complex operator * (const complex &e) {
+		return complex(r * e.r - i * e.i, r * e.i + i * e.r);
+	}
+};
+
+complex x[2 * max_n + 5];
+
+
+void change(complex *y, int len) {
+	int i, j, k;
+	for (i = 1, j = len / 2; i < len - 1; i++) {
+		if (i < j) std::swap(y[i], y[j]);
+		k = len / 2;
+		while (j >= k) {
+			j -= k;
+			k >>= 1;
+		}
+		if (j < k) j +=k;
+	}
 }
 
-int fast_frounier_transformation(int sign, cplx * data, int n) {
-	int d = 1 + __builtin_clz(n);
-	double theta = sign * 2.0 * PI / n;
-	for (int m = n; m >= 2; m >>= 1, theta *= 2) {
-		cplx tri = cplx(cos(theta), sin(theta));
-		cplx w = cplx(1, 0);
-		for (int i = 0, mh = m >> 1; i < mh; i++) {
-			for (int j = i; j < n; j += m) {
-				int k = j + mh;
-				cplx tmp = data[j] - data[k];
-				data[j] += data[k];
-				data[k] = w * tmp;
+void fast_founier_transformation(complex *y, int len, int sign) {
+	change(y, len);
+	for (int h = 2; h <= len; h <<= 1) {
+		complex wn(cos(- sign * 2 * PI / h), sin (- sign * 2 * PI / h));
+		for (int j = 0; j < len; j += h) {
+			complex w(1, 0);
+			for (int k = j; k < j + h / 2; k++) {
+				complex u = y[k] , t = w * y[k + h / 2];
+				y[k] = u + t;
+				y[k + h / 2] = u - t;
+				w = w * wn;
 			}
-			w *= tri;
 		}
 	}
-	for (int i = 0; i < n; i++) {
-		int j = intrev(i) >> d;
-		if (j < i) swap(data[i], data[j]);
-	}
-	if (sign == -1)
-		for (int i = 0; i < n; i++) data[i] = cplx(real(data[i]) / n, imag(data[i]));
-	return 0;
+	if (sign == -1) for (int i = 0; i < len; i++) y[i].r /= len;
 }
 
-int num[max_n];
-int count[max_n];
-long long ans[max_n];
-cplx x[2 * max_n];
+int num[max_n + 5];
+int count[max_n + 5];
+long long ans[2 * max_n + 5];
 
 int main () {
 	int n;
 	scanf("%d", &n);
-	std::memset(count, 0, sizeof(count));
+	memset(count, 0, sizeof(count));
 	int z = 0;
 	for (int i = 0; i < n; i++) {
 		scanf("%d", &num[i]);
-		if (num[i] == 0) z++;
-		count[num[i] + max_num]++;
+		if (!num[i]) z++;
+		count[num[i] + max]++;
 	}
 	int len = 1;
 	while (len < max_n) len <<= 1;
-	for (int i = 0; i < max_n; i++) x[i] = cplx(1.0 * count[i], 0.0);
-	fast_frounier_transformation(1, x, len);
+	for (int i = 0; i < max_n; i++) {
+		x[i].r = 1.0 * count[i];
+		x[i].i = 0;
+	}
+	fast_founier_transformation(x, len, 1);
 	for (int i = 0; i < len; i++) x[i] = x[i] * x[i];
-	fast_frounier_transformation(-1, x, len);
-	std::memset(ans, 0, sizeof(ans));
-	for (int i = 0; i < len; i++) ans[i] = (long long)(real(x[i]) + 0.5);
-	for (int i = 0; i < n; i++) ans[2 * (num[i] + max_num)]--;
+	fast_founier_transformation(x, len, -1);
+	memset(ans, 0, sizeof(ans));
+	for (int i = 0; i < len; i++) ans[i] = (long long) (x[i].r + 0.5);
+	for (int i = 0; i < n; i++) ans[2 * (num[i] + max)]--;
 	long long aans = 0;
 	for (int i = 0; i < n; i++) {
-		aans += ans[num[i] + max_num * 2];
-		if (num[i] == 0) aans -= (z - 1) * 2;
+		aans += ans[num[i] + max * 2];
+		aans -= (z - (num[i] == 0)) * 2;
 	}
 	printf("%lld\n", aans);
 	return 0;
