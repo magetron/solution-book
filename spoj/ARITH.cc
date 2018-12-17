95148694319df12f26c480ef35c62d25262cda48
Patrick Wu <magetron1@gmail.com>
2018-12-17 22:04:37 +0000

    add display for ARITH

--- spoj/ARITH.cc
+++ spoj/ARITH.cc
@@ -4,26 +4,54 @@
 
 using namespace std;
 
+int max (int a, int b) {
+	if (a > b) return a; else return b;
+}
 
 bool is_operand (char c) {
 	if ( (c == '+') || (c == '-') || (c == '*') ) return true;
 	else return false;
 }
 
+void upper_output (char* a, char* b, char op) {
+	if ( (op == '+') || (op == '-') ) { 
+		int a_len = strlen(a);
+		int b_len = strlen(b);
+		if (a_len > b_len) {
+			int gap = a_len - b_len;
+			cout << " ";
+			cout << a << endl;
+			cout << op;
+			for (int i = 0; i < gap; i++) cout << " ";
+			cout << b << endl;
+		} else {
+			int gap = b_len - a_len;
+			cout << " ";
+			for (int i = 0; i < gap; i++) cout << " ";
+			cout << a << endl;
+			cout << op;
+			cout << b << endl;
+		}
+	}
+}
+
+
 void do_work () {
 	char expression[expression_length];
 	char a[number_length], b[number_length];
+	char op;
+	memset(a, 0, sizeof(a));
+	memset(b, 0, sizeof(b));
 	cin >> expression;
 	int exp_len = strlen(expression);
 	for (int i = 0; i < exp_len; i++) if (is_operand(expression[i])) {
+		op = expression[i];
 		strncpy(a, expression, i);
-		strncpy(b, expression + i + 1, exp_len - i);
+		strncpy(b, expression + i + 1, exp_len - i - 1);
+		break;
 	}
-	cout << a << endl;
-	cout << b << endl;
+	upper_output(a, b, op);
 }
-	
-
 
 int main () {
 	ios_base::sync_with_stdio(false);
