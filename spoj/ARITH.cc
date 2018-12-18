3d76037d13f8cfd78a662875cc3aa2bbdf554dd7
Patrick Wu <magetron1@gmail.com>
2018-12-18 16:55:14 +0000

    complete plus and minus

--- spoj/ARITH.cc
+++ spoj/ARITH.cc
@@ -32,8 +32,76 @@ void upper_output (char* a, char* b, char op) {
 			cout << op;
 			cout << b << endl;
 		}
+		int line_len = max(a_len, b_len) + 1;
+		for (int i = 0; i < line_len; i++) cout << '-';
+		cout << endl;
 	}
 }
+
+void prec_plus (char *a, char *b) {
+	short int a_num[number_length];
+	short int b_num[number_length];
+	memset(a_num, -1, sizeof(a_num));
+	memset(b_num, -1, sizeof(b_num));
+	int a_len = strlen(a);
+	int b_len = strlen(b);
+	for (int i = a_len - 1; i >= 0; i--) a_num[a_len - i - 1] = a[i] - 48;
+	for (int i = b_len - 1; i >= 0; i--) b_num[b_len - i - 1] = b[i] - 48;
+	int result[number_length];
+	memset(result, 0, sizeof(result));
+	int result_len = max(a_len, b_len);
+	for (int i = 0; i < result_len; i++) {
+		int tmp_a, tmp_b;
+		if (i > a_len - 1) tmp_a = 0; else tmp_a = a_num[i];
+		if (i > b_len - 1) tmp_b = 0; else tmp_b = b_num[i];
+		result[i] += tmp_a + tmp_b;
+		if (result[i] >= 10) {
+			result[i] -= 10;
+			result[i + 1] += 1;
+		}
+	}
+	if (result[result_len] > 0) result_len++; else cout << " ";
+	for (int i = result_len - 1; i >= 0; i--) cout << result[i];
+	cout << endl;
+	return;
+}
+
+void prec_minus (char *a, char *b) {
+	short int a_num[number_length];
+	short int b_num[number_length];
+	memset(a_num, -1, sizeof(a_num));
+	memset(b_num, -1, sizeof(b_num));
+	int a_len = strlen(a);
+	int b_len = strlen(b);
+	for (int i = a_len - 1; i >= 0; i--) a_num[a_len - i - 1] = a[i] - 48;
+	for (int i = b_len - 1; i >= 0; i--) b_num[b_len - i - 1] = b[i] - 48;
+	int result[number_length];
+	memset(result, 0, sizeof(result));
+	int result_len = max(a_len, b_len);
+	for (int i = 0; i < result_len; i++) {
+		int tmp_a, tmp_b;
+		if (i > a_len - 1) tmp_a = 0; else tmp_a = a_num[i];
+		if (i > b_len - 1) tmp_b = 0; else tmp_b = b_num[i];
+		result[i] += tmp_a - tmp_b;
+		if (result[i] < 0) {
+			result[i] += 10;
+			result[i + 1] -= 1;
+		}
+	}
+	cout << " ";
+	bool prefix_zero = true;
+	for (int i = result_len - 1; i > 0; i--) {
+		if (result[i] > 0) prefix_zero = false; 
+		if (prefix_zero) cout << " "; else cout << result[i];
+	}
+	cout << result[0] << endl;
+}
+
+void result (char *a, char *b, char op) {
+	if ( op == '+' ) prec_plus(a, b);
+	if ( op == '-' ) prec_minus(a, b);
+}
+		
 
 
 void do_work () {
@@ -51,6 +119,8 @@ void do_work () {
 		break;
 	}
 	upper_output(a, b, op);
+	result(a, b, op);
+	cout << endl;
 }
 
 int main () {
