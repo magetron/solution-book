62e7233a4497844070e6e88d1f730f0d9741505d
Patrick Wu <magetron1@gmail.com>
2018-12-29 03:06:08 +0000

    finished FCTRL

---
+++ spoj/FCTRL.cc
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void do_work () {
+	long long n;
+	cin >> n;
+	int zeros = 0;
+	int fives = n / 5;
+	while (fives > 0) {
+		zeros += fives;
+		fives /= 5;
+	}
+	cout << zeros << endl;
+}	
+
+int main () {
+	ios_base :: sync_with_stdio(false);
+	cin.tie(NULL);	
+	int t;
+	cin >> t;
+	for (int i = 0; i < t; i++) do_work();
+	return 0;
+}
