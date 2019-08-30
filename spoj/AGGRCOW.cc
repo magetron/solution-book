0fb2d48864a094f318b12d0fcd0e193b8fb7ebe3
Patrick Wu <magetron1@gmail.com>
2019-08-30 20:37:37 +0100

    working on AGGRCOW

---
+++ spoj/AGGRCOW.cc
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+#define MAX_N 100005
+#define MAX_X 1000000000
+
+void do_work () {
+	int n, c;
+	long long stall[MAX_N];
+	for (int i = 0; i < n; i++) cin >> stall[i];
+	// sort the stall
+	// find f(x) for each x distance
+	// binary search to narrow down
+}
+
+
+
+int main () {
+	ios_base :: sync_with_stdio(false);
+	cin.tie(NULL);
+	
+	int n;
+	cin >> n;
+	for (int i = 0; i < n; i++) do_work();
+
+	return 0;
+}
