3934846a0c7e7a11cc395326a510e3819e9f6575
Patrick Wu <magetron1@gmail.com>
2019-09-02 19:04:35 +0100

    solved INVCNT

--- spoj/INVCNT.cc
+++ spoj/INVCNT.cc
@@ -2,34 +2,46 @@
 
 using namespace std;
 
+void convert (vector<int>& arr) {
+	int len = arr.size();
+	vector<int> cpy = arr;
+	sort(cpy.begin(), cpy.end());
+	for (int i = 0; i < len; i++) arr[i] = lower_bound(cpy.begin(), cpy.end(), arr[i]) - cpy.begin() + 1;
+}
+
+void update (vector<int>& bit, int n, int i, int val) {
+	while (i <= n) {
+		bit[i] += val;
+		i += i & (- i);
+	}
+}
+
+int get_sum (vector<int>& bit, int i) {
+	int ans = 0;
+	while (i > 0) {
+		ans += bit[i];
+		i -= i & (- i);
+	}
+	return ans;
+}
+
 void do_work () {
     int n;
     cin >> n;
-    list<int> arr;
-    long long ans = 0;
-    for (int i = 0; i < n; i++) {
-        int new_element;
-        cin >> new_element;
-		if (i == 0) {
-			arr.push_back(new_element);
-			continue;
-		}
-        int dist = 0;
-		bool found = false;
-        for (auto it = arr.begin(); it != arr.end(); it++) {
-            dist++;
-            if (*it > new_element) {
-                ans += ((i + 1) - dist);
-                arr.insert(it, new_element);
-				found = true;
-                break;
-            }
-        }
-		if (!found) arr.push_back(new_element);
-		//for (auto it : arr) cout << it << " ";
-		//cout << endl;
-    }
-    cout << ans << endl;
+	vector<int> arr(n, 0);
+	for (int i = 0; i < n; i++) cin >> arr[i];
+	convert(arr);
+	//for (auto it = arr.begin(); it != arr.end(); it++) cout << *it << " ";
+	//cout << endl;
+
+	vector<int> bit(arr.size() + 1, 0);
+	long ans = 0;
+	for (int i = arr.size() - 1; i >= 0; i--) {
+		ans += get_sum(bit, arr[i] - 1);
+		update(bit, arr.size(), arr[i], 1);
+	}
+	cout << ans << endl;
+	return;
 }
 
 int main () {
@@ -42,4 +54,4 @@ int main () {
     fclose(stdin);
     return 0;
 }
-
+ 
