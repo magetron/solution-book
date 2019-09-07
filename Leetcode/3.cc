75b7d76d4c63a32f5a6879ccbcd9ed79b8b34d1b
Patrick Wu <magetron1@gmail.com>
2019-09-07 16:00:07 +0100

    update 3

--- Leetcode/3.cc
+++ Leetcode/3.cc
@@ -5,19 +5,28 @@ using namespace std;
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-		set<char> dict;
-		int len = s.length();
+		unordered_set <char> dict;
 		int i = 0;
 		int j = 0;
-		int ans = 0;
-		while (i < len - 1 && j < len) {
-			auto f = dict.find(s[j]);
-			if (f != dict.end()) dict.erase(s[i++]);
-			else {
-				dict.insert(j++);
-				ans = max(ans, j - i);
-			}
+		int max = 0;
+		while (i <= j && j < s.length()) {
+			//cout << i << " " << j << endl;
+			auto found = dict.find(s.at(j));
+			if (found == dict.end()) 
+				dict.insert(s.at(j++));
+			else 
+				dict.erase(s.at(i++));
+			if (j - i > max) max = j - i;
 		}
-		return ans;	
+		return max;
     }
 };
+
+int main () {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	string s("abcabcbb");
+	Solution sol;
+	cout << sol.lengthOfLongestSubstring(s);
+	return 0;
+}
