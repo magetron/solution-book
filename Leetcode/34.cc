4487c9aafa93ee19c3574c68834073b9009a0ddc
Patrick Wu <magetron1@gmail.com>
2019-09-14 14:22:32 +0100

    refactor 34

--- Leetcode/34.cc
+++ Leetcode/34.cc
@@ -3,44 +3,47 @@
 using namespace std;
 
 class Solution {
-	public:
-		vector<int> searchRange (vector<int>& nums, int target) {
-			if (nums.size() == 0) return vector<int>{-1, -1};
-			int l = 0;
-			int r = nums.size() - 1;
-			while (l < r) {
-            	//cout << l << " " << r << endl;
-				int mid = l + (r - l) / 2;
-				if (nums[mid] == target) break;
-				if (nums[mid] < target) l = mid + 1;
-				else r = mid;
-			}
-            if (l >= nums.size() || r >= nums.size()) return vector<int>{-1, -1};
-            int mid = l + (r - l) / 2;
-			if (nums[mid] != target) return vector<int>{-1, -1};
-			l = 0;
-			r = mid;
-			//cout << r << endl;
-			int l_mid;
-			while (l < r) {
-				int mid = l + (r - l) / 2;
-				if (nums[mid] < target && nums[mid + 1] == target) break;
-				if (nums[mid] < target) l = mid + 1;
-				else r = mid;
-			}
-			if (r == 0) l_mid = 0; else l_mid = l + (r - l) / 2 + 1;
-			l = mid + 1;
-			if (l >= nums.size() || nums[l] > target) return vector<int>{l_mid, mid};
-			r = nums.size() - 1;
-			while (l < r) {
-				int mid = l + (r - l) / 2;
-				if (nums[mid] == target && nums[mid + 1] > target) break;
-				if (nums[mid] == target) l = mid + 1;
-				else r = mid;
-			}
-			int r_mid = l + (r - l) / 2;
-			return vector<int>{l_mid, r_mid};
-		}
+public:
+    vector<int> searchRange(vector<int>& nums, int target) {
+       int l = 0;
+	   int r = nums.size();
+	   int pos = - 1;
+	   while (l < r) {
+		   int mid = l + (r - l) / 2;
+		   if (nums[mid] == target) {
+			   pos = mid;
+			   break;
+		   }
+		   else if (nums[mid] > target) r = mid;
+		   else l = mid + 1;
+	   }
+	   if (pos == -1) return vector<int>{-1, -1};
+	   l = 0;
+	   r = pos + 1;
+	   int l_pos = -1;
+	   while (l < r) {
+		   int mid = l + (r - l) / 2;
+		   if (nums[mid] == target && (mid - 1 < 0 || nums[mid - 1] < target)) {
+			   l_pos = mid;
+			   break;
+		   } 
+		   else if (nums[mid] == target) r = mid;
+		   else l = mid + 1;
+	   }
+	   l = pos;
+	   r = nums.size();
+	   int r_pos = -1;
+	   while (l < r) {
+		   int mid = l + (r - l) / 2;
+		   if (nums[mid] == target && (mid + 1 == nums.size() || nums[mid + 1] > target)) {
+			   r_pos = mid;
+			   break;
+		   }
+		   else if (nums[mid] == target) l = mid + 1;
+		   else r = mid;
+	   }
+	   return vector<int>{l_pos, r_pos};
+    }
 };
 
 int main () {
