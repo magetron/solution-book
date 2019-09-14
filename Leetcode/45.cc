12687d83d06915ee6af5daf42a8db9cce815bdf3
Patrick Wu <magetron1@gmail.com>
2019-09-14 20:15:38 +0100

    solved 45 more efficiently

--- Leetcode/45.cc
+++ Leetcode/45.cc
@@ -4,26 +4,20 @@ using namespace std;
 
 class Solution {
 public:
-	void print (vector<int>& a) {
-		for (auto i : a) cout << i << " ";
-		cout << endl;
-	}
-
     int jump(vector<int>& nums) {
-		vector<int> dp(nums.size(), INT_MAX);
-		int cur = 0;
 		int prev = 0;
+        int cur = 0;
+		int new_cur = 0;
 		int steps = 0;
 		while (cur < nums.size() - 1) {
-			int new_cur = 0;
-			for (int i = prev; i <= cur; i++) if (nums[i] + i > new_cur) new_cur = nums[i] + i;
+			steps++;
+			for (int i = prev; i <= cur; i++) new_cur = max(new_cur, i + nums[i]);
+			if (cur == new_cur) return -1;
 			prev = cur;
 			cur = new_cur;
-			steps++;
-			if (prev == cur) return -1;
 		}
 		return steps;
-	}
+    }
 };
 
 int main () {
