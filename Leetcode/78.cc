00f842a626dcef5ebf4743d3f59520e8695276e3
Patrick Wu <magetron1@gmail.com>
2019-09-17 19:28:20 +0100

    solved 78 faster & easier

--- Leetcode/78.cc
+++ Leetcode/78.cc
@@ -2,34 +2,21 @@
 
 using namespace std;
 
-
 class Solution {
 public:
-
-	void helper (vector<int>& n, int l, int k, vector<vector<int>>& ans, bool mid) {
-		//for (auto i : n) cout << i << " ";
-		//cout << endl;
-		if (l == k) {
-			ans.push_back(vector<int>(n.begin(), n.begin() + k));
-			if (!mid) ans.push_back(vector<int>(n.begin() + k, n.end()));
-			return;
-		}
-		for (int i = l; i < n.size(); i++) 
-			if (l == 0 || n[i] > n[l - 1]) {
-				swap(n[l], n[i]);
-				helper(n, l + 1, k, ans, mid);
-				swap(n[i], n[l]);
+    vector<vector<int>> subsets(vector<int>& nums) {
+    	vector<vector<int>> ans;
+		ans.push_back(vector<int>());
+		for (int i = 0; i < nums.size(); i++) { 
+			int org_size = ans.size();
+    		for (int j = 0; j < org_size; j++) {
+				auto cpy = ans[j];
+				cpy.push_back(nums[i]);
+				ans.push_back(cpy);
 			}
-	}
-		
-	vector<vector<int>> subsets(vector<int>& nums) {
-		sort(nums.begin(), nums.end());
-   		vector<vector<int>> ans;
-		for (int i = 0; i <= nums.size() / 2; i++) 
-			if (nums.size() % 2 == 0 && i == nums.size() / 2) helper(nums, 0, i, ans, true);
-			else helper(nums, 0, i, ans, false);
+		}
 		return ans;
-    }
+	}
 };
 
 int main () {
@@ -37,10 +24,10 @@ int main () {
 	cin.tie(NULL);
 	cout.tie(NULL);
 	Solution s;
-	vector<int> nums {1,2,3,4};
+	vector<int> nums{1,2,3};
 	auto ans = s.subsets(nums);
 	for (auto as : ans) {
-		for (auto a : as) cout << a << " ";
+		for (auto i : as) cout << i << " ";
 		cout << endl;
 	}
 	return 0;
