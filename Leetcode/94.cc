a10fc10269d30c1ab351a80385d19fdb74865556
Patrick Wu <magetron1@gmail.com>
2019-09-17 20:48:46 +0100

    solved 94

---
+++ Leetcode/94.cc
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+	
+	void helper (TreeNode* root, vector<int>& ans) {
+		if (root == NULL) return;
+		helper(root -> left, ans);
+		ans.push_back(root -> val);
+		helper(root -> right, ans);
+	}
+
+    vector<int> inorderTraversal(TreeNode* root) {
+    	vector<int> ans;
+		helper(root, ans);
+		return ans;
+	}
+};
