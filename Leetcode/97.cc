1a095d9f6d1f59a85a1b1c7d9fe9aea41e637e94
Patrick Wu <magetron1@gmail.com>
2019-09-18 15:15:21 +0100

    solved 97

---
+++ Leetcode/97.cc
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+class Solution {
+public:
+    bool isInterleave(string s1, string s2, string s3) {
+		if (s1.length() + s2.length() != s3.length()) return false;
+		vector<bool> dp(s2.length() + 1, false);
+		for (int i = 0; i <= s1.length(); i++) 
+			for (int j = 0; j <= s2.length(); j++)
+				if (i == 0 && j == 0) dp[j] = true;
+				else if (i == 0) dp[j] = dp[j - 1] && s2[j - 1] == s3[j - 1];
+				else if (j == 0) dp[j] = dp[j] && s1[i - 1] == s3[i + j - 1];
+				else dp[j] = (dp[j - 1] && s2[j - 1] == s3[i + j - 1]) || (dp[j] && s1[i - 1] == s3[i + j - 1]);
+		return dp[s2.length()];
+    }
+};
