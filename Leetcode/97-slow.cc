0cd821482782d5e14609e579325ed23471c87b86
Patrick Wu <magetron1@gmail.com>
2019-09-18 15:22:55 +0100

    refactor 97 slow

--- Leetcode/97-slow.cc
+++ Leetcode/97-slow.cc
@@ -1,48 +1,49 @@
 #include <bits/stdc++.h>
+#define si short int
 
 using namespace std;
 
 class Solution {
 	public:
-		bool dfs (string& s1, int l1, string& s2, int l2, string& s3 ,int l3, vector<vector<vector<int>>>& memo) {
+		bool dfs (string& s1, int l1, string& s2, int l2, string& s3, vector<vector<si>>& memo) {
 			//for (int i = 0; i < l1; i++) cout << s1[i]; cout << endl;
 			//for (int i = 0; i < l2; i++) cout << s2[i]; cout << endl;
 			//for (int i = 0; i < l3; i++) cout << s3[i]; cout << endl;
 			//cout << l1 << " " << l2 << " " << l3 << endl;
 			//cout << memo[l1][l2][l3] << endl;  
 			//cout << "-----" << endl;
-			if (memo[l1][l2][l3] != -1) return memo[l1][l2][l3] == 1;
-			if (l1 == s1.length() && l2 == s2.length() && l3 == s3.length()) {
-				memo[l1][l2][l3] = 1;
+			if (memo[l1][l2] != -1) return memo[l1][l2] == 1;
+			if (l1 == s1.length() && l2 == s2.length()) {
+				memo[l1][l2] = 1;
 				return true;
 			}
 			if (l1 == s1.length()) {
 				bool res;
-				if (l2 == s2.length() || l3 == s3.length() || s2[l2] != s3[l3]) res = false; else res = dfs(s1, l1, s2, l2 + 1, s3, l3 + 1, memo);
-				memo[l1][l2][l3] = res;
+				if (l2 == s2.length() || s2[l2] != s3[l1 + l2]) res = false; else res = dfs(s1, l1, s2, l2 + 1, s3, memo);
+				memo[l1][l2] = res;
 				return res;
 			}
 			if (l2 == s2.length()) {
 				bool res;
-				if (l1 == s1.length() || l3 == s3.length() || s1[l1] != s3[l3]) res = false; else res = dfs(s1, l1 + 1, s2, l2, s3, l3 + 1, memo);
-				memo[l1][l2][l3] = res;
+				if (l1 == s1.length() || s1[l1] != s3[l1 + l2]) res = false; else res = dfs(s1, l1 + 1, s2, l2, s3, memo);
+				memo[l1][l2] = res;
 				return res;
 			}
 			bool branch1 = false, branch2 = false; 
-			if (s1[l1] == s3[l3]) branch1 = dfs(s1, l1 + 1, s2, l2, s3, l3 + 1, memo);
-			memo[l1][l2][l3] = branch1;
+			if (s1[l1] == s3[l1 + l2]) branch1 = dfs(s1, l1 + 1, s2, l2, s3, memo);
+			memo[l1][l2] = branch1;
 			if (branch1) return true;
-			if (s2[l2] == s3[l3]) branch2 = dfs(s1, l1, s2, l2 + 1, s3, l3 + 1, memo);
-			memo[l1][l2][l3] = branch2;
+			if (s2[l2] == s3[l1 + l2]) branch2 = dfs(s1, l1, s2, l2 + 1, s3, memo);
+			memo[l1][l2] = branch2;
 			if (branch2) return true;
-			memo[l1][l2][l3] = 0;
+			memo[l1][l2] = 0;
 			return false;
 		}
 		
 		bool isInterleave (string s1, string s2, string s3) {
 			if (s1.length() + s2.length() != s3.length()) return false;
-			vector<vector<vector<int>>> memo(s1.length() + 1, vector<vector<int>>(s2.length() + 1, vector<int>(s3.length() + 1, -1)));
-			return dfs(s1, 0, s2, 0, s3, 0, memo);
+			vector<vector<si>> memo(s1.length() + 1, vector<si>(s2.length() + 1, -1));
+			return dfs(s1, 0, s2, 0, s3, memo);
 		}
 };
 
@@ -52,7 +53,7 @@ int main () {
 	cout.tie(NULL);
 	string s1("aabcc");
 	string s2("dbbca");
-	string s3("aadbbbaccc");
+	string s3("aadbbbccac");
 	Solution s;
 	cout << (s.isInterleave(s1, s2, s3) ? "true" : "false") << endl;
 	return 0;
