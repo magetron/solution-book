3927dae5a8168ea7b470216dc9659837c23b3e31
Patrick Wu <magetron1@gmail.com>
2019-09-19 10:06:57 +0100

    solved 107 108

---
+++ Leetcode/107.cc
@@ -0,0 +1,67 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct TreeNode {
+	int val;
+	TreeNode *left;
+	TreeNode *right;
+	TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution {
+public:
+    vector<vector<int>> levelOrderBottom(TreeNode* root) {
+		if (root == NULL) return vector<vector<int>>();
+		queue<TreeNode *> q;
+		q.push(root);
+		int count = 1;
+		vector<vector<int>> ans;
+		while (count) {
+			vector<int> lvl;
+			int new_count = 0;
+			for (int i = 0; i < count; i++) {
+				TreeNode *cur = q.front();
+				q.pop();
+				if (cur -> left != NULL) {
+					q.push(cur -> left);
+					new_count++;
+				}
+				if (cur -> right != NULL) {
+					q.push(cur -> right);
+					new_count++;
+				}
+				lvl.push_back(cur -> val);
+			}
+			ans.push_back(lvl);
+			count = new_count;
+		}
+		reverse(ans.begin(), ans.end());
+		return ans;
+    }
+};
+
+int main () {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	TreeNode *n1 = new TreeNode(3);
+	TreeNode *n2 = new TreeNode(9);
+	TreeNode *n3 = new TreeNode(20);
+	TreeNode *n4 = new TreeNode(15);
+	TreeNode *n5 = new TreeNode(7);
+	n1 -> left = n2;
+	n1 -> right = n3;
+	n3 -> left = n4;
+	n3 -> right = n5;
+		
+	Solution s;
+	auto ans = s.levelOrder(n1);
+	for (auto as : ans) {
+		for (auto a : as) cout << a << " ";
+		cout << endl;
+	}
+
+	return 0;
+}
---
+++ Leetcode/108.cc
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+	TreeNode* helper (vector<int>& nums, int l, int r) {
+		if (l >= r) return NULL;
+		int mid = l + (r - l) / 2;
+		TreeNode* root = new TreeNode(nums[mid]);
+		root -> left = helper(nums, l, mid);
+		root -> right = helper(nums, mid + 1, r);
+		return root;
+	}
+
+    TreeNode* sortedArrayToBST(vector<int>& nums) {
+		return helper(nums, 0, nums.size());		
+    }
+};
