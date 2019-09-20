80067d27cbfa4209e6092cad585c6fddd219d4e0
Patrick Wu <magetron1@gmail.com>
2019-09-20 12:32:36 +0100

    solved 117

---
+++ Leetcode/117.cc
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+static int fast_io = []() { ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr); return 0; }();
+
+class Solution {
+	public:
+		Node* connect(Node* root) {
+			Node* ret_root = root;
+			Node* head = nullptr;
+			Node* lead = nullptr;
+			Node* cur = root;
+			while (cur) {
+				while (cur) {
+					if (cur -> left) {
+						if (lead) lead -> next = cur -> left;
+						else head = cur -> left;
+						lead = cur -> left;
+					}
+					if (cur -> right) {
+						if (lead) lead -> next = cur -> right;
+						else head = cur -> right;
+						lead = cur -> right;
+					}
+					cur = cur -> next;
+				}
+				cur = head;
+				head = nullptr;
+				lead = nullptr;
+			}
+			return root;
+		}
+};
+
+
