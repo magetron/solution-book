2564e3e3bd14e4733c27f35ed469a3798ab1c966
Patrick Wu <magetron1@gmail.com>
2019-09-21 19:41:13 +0100

    refactor 44

--- Leetcode/44.cc
+++ Leetcode/44.cc
@@ -4,21 +4,20 @@ using namespace std;
 
 class Solution {
 public:
-    bool isMatch(string s, string p) {
-        int i = 0, j = 0, i_star = - 1, j_star = - 1;
-		while (i < s.length()) {
-			if (j < p.length() && (s.at(i) == p.at(j) || p.at(j) == '?')) { i++; j++; }
-			else if (j < p.length() && p.at(j) == '*') {
-				i_star = i;
-				j_star = j;
-				j++;
-			}
-			else if (i_star != -1) {
-				i = ++i_star;
-				j = j_star + 1;
+	bool isMatch(string s, string p) {
+    	int i = 0, j = 0;
+		int iStar = -1, jStar = -1;
+		while (i < s.length()) 
+			if (j < p.length() && (s[i] == p[j] || p[j] == '?')) {
+				i++; j++;
+			} else if (j < p.length() && p[j] == '*') {
+				iStar = i;
+				jStar = j++;
+			} else if (jStar != - 1) {
+				i = ++iStar;
+				j = jStar + 1;
 			} else return false;
-		}
-		while (j < p.length()) if (p.at(j) != '*') return false; else j++;
+		while (j < p.length() && p[j] == '*') j++;
 		return j == p.length();
     }
 };
