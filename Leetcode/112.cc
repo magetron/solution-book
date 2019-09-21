72ee59eb098babdbf590a620f3c0432bdc2953b2
Patrick Wu <magetron1@gmail.com>
2019-09-22 00:11:21 +0100

    refactor 112

--- Leetcode/112.cc
+++ Leetcode/112.cc
@@ -1,12 +1,21 @@
 #include <bits/stdc++.h>
+#pragma GCC optimize ("Ofast")
 
 using namespace std;
+
+static int fast_io = [] () {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+	return 0;
+} ();
 
 class Solution {
 public:
     bool hasPathSum(TreeNode* root, int sum) {
-        if (root == NULL) return false;
-		if (root -> left == NULL && root -> right == NULL) return root -> val == sum;
-		else return hasPathSum(root -> left, sum - root -> val) || hasPathSum(root -> right , sum - root -> val);
+        if (root == nullptr) return false;
+    	if (root -> left == nullptr && root -> right == nullptr) return root -> val == sum;
+		else return (root -> left == nullptr ? false : hasPathSum(root -> left, sum - root -> val)) ||
+					(root -> right == nullptr ? false : hasPathSum(root -> right, sum - root -> val));
     }
 };
