11107d2107913d9994dccf4eb096c17c251915d1
Patrick Wu <magetron1@gmail.com>
2019-09-21 23:53:00 +0100

    refactor 92

--- Leetcode/92.cc
+++ Leetcode/92.cc
@@ -1,4 +1,5 @@
 #include <bits/stdc++.h>
+#pragma GCC optimize ("Ofast")
 
 using namespace std;
 
@@ -8,34 +9,34 @@ struct ListNode {
 	ListNode(int x) : val(x), next(NULL) {}
 };
 
+
+static int fast_io = [] () {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+	return 0;
+} ();
+
 class Solution {
 public:
-    ListNode* reverseBetween(ListNode* head, int m, int n) {
-        if (head == NULL || m > n) return NULL;
-
-		ListNode *dummy = new ListNode(0);
-		dummy -> next = head;
-		head = dummy;
-
+	ListNode* reverseBetween(ListNode* head, int m, int n) {
+    	ListNode* fake = new ListNode(0);
+		fake -> next = head;
+		head = fake;
 		for (int i = 0; i < m - 1; i++) head = head -> next;
-		ListNode *prev = head;
-
-		ListNode *nodeA = head -> next;
-		ListNode *nodeB = head -> next -> next;
-		ListNode *nodeStart = nodeA;
-
-		int count = 0;
-		while (count < n - m) {
-			nodeA -> next = nodeB -> next;
-			nodeB -> next = nodeStart;
-			nodeStart = nodeB;
-			nodeB = nodeA -> next;
-			count++;
+		ListNode* pre = head;
+		ListNode* nodea = head -> next;
+		ListNode* nodeb = head -> next -> next;
+		ListNode* nodehead = nodea;
+		int count = m;
+		while (count < n) {
+			nodea -> next = nodeb -> next;
+			nodeb -> next = nodehead;
+			nodehead = nodeb;
+			nodeb = nodea -> next;
+            count++;
 		}
-
-		prev -> next = nodeStart;
-
-		return dummy -> next;
-
+		pre -> next = nodehead;
+		return fake -> next;
     }
 };
