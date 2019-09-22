1ae7fe7d78ca6927794c58210cc4084e0e5fe3b7
Patrick Wu <magetron1@gmail.com>
2019-09-22 11:15:58 +0100

    refactor 113

--- Leetcode/113.cc
+++ Leetcode/113.cc
@@ -1,29 +1,33 @@
 #include <bits/stdc++.h>
+#pragma GCC optimize ("Ofast")
 
 using namespace std;
 
+static int fast_io = [] () {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+	return 0;
+} ();
+
 class Solution {
 public:
-	void helper(TreeNode* root, int sum, vector<int>& path, vector<vector<int>>& ans) {
-		if (root == NULL) return;
-		if (root -> left == NULL && root -> right == NULL && root -> val == sum) {
-			path.push_back(root -> val);
+	void helper (TreeNode* root, int sum, vector<int>& path, vector<vector<int>>& ans) {
+		path.push_back(root -> val);
+		if (root -> left == nullptr && root -> right == nullptr && root -> val == sum) {
 			ans.push_back(path);
-			path.pop_back();
+            path.pop_back();
 			return;
 		}
-		path.push_back(root -> val);
-		helper(root -> left, sum, path, ans);
-		helper(root -> right, sum, path, ans);
+		if (root -> left != nullptr) helper(root -> left, sum - root -> val, path, ans);
+		if (root -> right != nullptr) helper(root -> right, sum - root -> val, path, ans);
 		path.pop_back();
-
-
-			
-	}	
+	}
 
     vector<vector<int>> pathSum(TreeNode* root, int sum) {
-		vector<vector<int>> ans;
+		if (root == nullptr) return vector<vector<int>>();
 		vector<int> path;
+		vector<vector<int>> ans;
 		helper(root, sum, path, ans);
 		return ans;
     }
