8036bc6b784aec51eda5732317e1d8a2b44dd925
Patrick Wu <magetron1@gmail.com>
2019-09-23 13:27:17 +0100

    solved 130

--- Leetcode/130.cc
+++ Leetcode/130.cc
@@ -14,27 +14,10 @@ class Solution {
 	public:
 		vector<pair<int, int>> dir{{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
 
-		bool investigate(vector<vector<char>>& board, int i, int j, vector<vector<bool>>& visited) {
-			//cout << "call" << i << " " << j << endl;
-			if (i < 0 || i >= board.size() || j < 0 || j >= board[0].size()) return false;
-			if (board[i][j] == 'X') return true;
-			bool res = true;
-			visited[i + 1][j + 1] = true;
-			for (int k = 0; k < 4; k++)
-				if (!visited[i + dir[k].first + 1][j + dir[k].second + 1])
-                    res = investigate(board, i + dir[k].first, j + dir[k].second, visited) && res;
-			return res;
-		}
-
-		void flip(vector<vector<char>>& board, int i, int j) {
-			if (i < 0 || i >= board.size() || j < 0 || j >= board[0].size() || board[i][j] == 'X') return;
-			board[i][j] = 'X';
-			//for (auto l : board) {
-			//	for (auto i : l) cout << i << " ";
-			//	cout << endl;
-			//}
-			//cout << "-----" << endl;
-			for (int k = 0; k < 4; k++) flip(board, i + dir[k].first, j + dir[k].second);
+		void mark(vector<vector<char>>& board, int i, int j) {
+			if (i < 0 || i >= board.size() || j < 0 || j >= board[0].size() || board[i][j] == 'X' || board[i][j] == '*') return;
+			board[i][j] = '*';
+			for (int k = 0; k < 4; k++) mark(board, i + dir[k].first, j + dir[k].second);
 		}
 
 		void solve(vector<vector<char>>& board) {
@@ -42,10 +25,18 @@ class Solution {
 			if (m == 0) return;
 			int n = board[0].size();
 			if (n == 0) return;
-			vector<vector<bool>> visited(m + 2, vector<bool>(n + 2, false));
+			for (int i = 0; i < m; i++) {
+				if (board[i][0] == 'O') mark(board, i, 0);
+				if (board[i][n - 1] == 'O') mark(board, i, n - 1);
+			}
+			for (int j = 0; j < n; j++) {
+				if (board[0][j] == 'O') mark(board, 0, j);
+				if (board[m - 1][j] == 'O') mark(board, m - 1, j);
+			}
 			for (int i = 0; i < m; i++)
-				for (int j = 0; j < n; j++) if (board[i][j] == 'O' && !visited[i + 1][j + 1]) 
-                    if (investigate(board, i, j, visited)) flip(board, i, j);
+				for (int j = 0; j < n; j++) 
+					if (board[i][j] == 'O') board[i][j] = 'X';
+					else if (board[i][j] == '*') board[i][j] = 'O';
 		}
 };
 
