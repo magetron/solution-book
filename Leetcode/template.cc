3b25a5831d912a574516f6128d41a38657de0e68
Patrick Wu <magetron1@gmail.com>
2019-09-30 22:49:40 +0100

    add template

---
+++ Leetcode/template.cc
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+#pragma GCC optimize ("Ofast")
+
+using namespace std;
+
+static int fast_io = [] () {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+	return 0;
+} ();
+
+
+
+
+
+int main () {
+	Solution s;
+
+	return 0;
+}
