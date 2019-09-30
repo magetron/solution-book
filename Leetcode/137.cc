2a1c6a0cec782f804cbc427e89a8b97dd8b373ce
Patrick Wu <magetron1@gmail.com>
2019-09-30 15:11:02 +0100

    refactor 137

--- Leetcode/137.cc
+++ Leetcode/137.cc
@@ -13,12 +13,12 @@ static int fast_io = [] () {
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-		int ones = 0, twos = 0;
-		for (auto i : nums) {
-			ones = (ones ^ i) & ~twos;
-			twos = (twos ^ i) & ~ones;
+        int i = 0, j = 0;
+		for (auto n : nums) {
+			i = i ^ (n & ~j);
+			j = j ^ (n & ~i);
 		}
-		return ones;
+		return i;
     }
 };
 
