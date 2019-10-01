10e2bc2e66d9d77bfbfa1f53ed575e65bead41c1
Patrick Wu <magetron1@gmail.com>
2019-10-01 15:23:46 +0100

    solved 191

--- Leetcode/191-slow.cc
+++ Leetcode/191-slow.cc
@@ -13,11 +13,9 @@ static int fast_io = [] () {
 class Solution {
 public:
     int hammingWeight(uint32_t n) {
+		bitset<32> bs(n);
 		int ans = 0;
-		while (n) {
-			n &= (n - 1);
-			ans++;
-		}
+		for (int i = 0; i < 32; i++) ans += (b[i] ? 1 : 0);
 		return ans;
     }
 };
