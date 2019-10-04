4785ef1026e5104b1fa2085b35d23deb989754c7
Patrick Wu <magetron1@gmail.com>
2019-10-04 13:20:35 +0100

    solved 215

---
+++ Leetcode/215.cc
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+#pragma GCC optimize ("Ofast")
+
+using namespace std;
+				
+
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+   		priority_queue<int, vector<int>, greater<int>> pq(nums.begin(), nums.begin() + k);
+		for (int i = k; i < nums.size(); i++) if (nums[i] > pq.top()) {
+			pq.pop();
+			pq.push(nums[i]);
+		}
+		return pq.top();
+    }
+}; 
+
