c8f728be9d3d77fdded76e61d10b200ec29dc026
Patrick Wu <magetron1@gmail.com>
2019-10-07 00:35:10 +0100

    solved 149 faster

--- Leetcode/149.cc
+++ Leetcode/149.cc
@@ -1,4 +1,5 @@
 #include<bits/stdc++.h>
+#define ll long long
 #pragma GCC optimize ("Ofast")
 
 using namespace std;
@@ -10,57 +11,33 @@ static int fast_io = [] () {
 	return 0;
 } ();
 
-struct hashfunc {
-	size_t operator () (const pair<int, int>& p) const {
-		return ((p.first & ~100007) | (p.second & 100007)) ^ ((p.first & 100007) | (p.second & ~100007)); 
-	}
-};
-
 class Solution {
 public:
     int maxPoints(vector<vector<int>>& points) {
-		if (points.size() <= 2) return points.size();
-		int ans = 0;
-		for (int i = 0; i < points.size() - 1; i++) {
-			unordered_map<pair<int, int>, int, hashfunc> dict;
-			int overlap = 0, vertical = 0, horizontal = 0;
-			for (int j = i + 1; j < points.size(); j++) {
-				if (points[i][0] == points[j][0] && points[i][1] == points[j][1]) overlap++;
-				else {
-					int a = (points[i][1] - points[j][1]);
-					int b = (points[i][0] - points[j][0]);
-					//cout << a << " " << b << endl;
-					if (a == 0) {
-						horizontal++;
-						continue;
-					} else if (b == 0) {
-						vertical++;
-						continue;
-					}
-					int g = gcd(a, b);
-					a /= g;
-					b /= g;
-					if (a < 0) { a = -a; b = -b; }
-					dict[make_pair(a, b)]++;
+        if (points.size() <= 2) return points.size();        
+		int ans = 2;
+    	for (int i = 1; i < points.size(); i++) {
+			int x1 = points[i][0];
+			int y1 = points[i][1];
+			int x0 = points[i - 1][0];
+			int y0 = points[i - 1][1];
+			int count = 0;
+			if (x1 == x0 && y1 == y0)
+				for (int j = 0; j < points.size(); j++) {
+					int x2 = points[j][0];
+					int y2 = points[j][1];
+					if (x1 == x2 && y1 == y2) count++;
 				}
-			}
-			//cout << "i = " << i << endl;
-			for (auto p : dict) {
-				//cout << p.first.first << " " << p.first.second << " "<< p.second << endl;
-				//cout << overlap << endl;
-				ans = max(ans, p.second + overlap + 1);
-			}
-			//cout << ans << endl;
-			ans = max(max(ans, vertical + overlap + 1), horizontal + overlap + 1);
+			else 
+				for (int j = 0; j < points.size(); j++) {
+					int x2 = points[j][0];
+					int y2 = points[j][1];
+					if ((ll)(x1 - x0) * (ll)(y2 - y1) == (ll)(x2 - x1) * (ll)(y1 - y0)) count++;
+				}
+			ans = max(ans, count);
 		}
-		return ans;		
-    }
+		return ans;
+	}
+
 };
 
-int main () {
-	Solution s;
-	//vector<vector<int>> points{{1,1}, {3,2}, {5,3}, {4,1}, {2,3}, {1,4}};
-	vector<vector<int>> points{{2,3}, {3,3}, {-5,3}};
-	cout << s.maxPoints(points) << endl;
-	return 0;
-}
