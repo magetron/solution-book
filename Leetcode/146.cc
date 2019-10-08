c5550537ea32d948ec07d113e2de3d96f975b421
Patrick Wu <magetron1@gmail.com>
2019-10-08 18:51:09 +0100

    refactor 146

--- Leetcode/146.cc
+++ Leetcode/146.cc
@@ -1,33 +1,43 @@
+#include<bits/stdc++.h>
+#pragma GCC optimize ("Ofast")
+
+using namespace std;
+
+static int fast_io = [] () {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 0;
+} ();
+
 class LRUCache {
 public:
+	list<pair<int, int>> ls;
+	unordered_map<int, list<pair<int,int>>::iterator> dict;
 	int cap;
-	list<pair<int, int>> nodes;
-	unordered_map<int, list<pair<int, int>>::iterator> hmap;
 
-    LRUCache(int capacity) {
-		cap = capacity;
+    LRUCache(int capacity) : cap(capacity) {    
     }
     
     int get(int key) {
-        auto found = hmap.find(key);
-		if (found != hmap.end()) {
-			int val = (*(*found).second).second;
-			put(key, val);
-			return val;
-		}
-		return -1;
+		auto found = dict.find(key);
+		if (found == dict.end()) return -1;
+		int val = (*(*found).second).second;
+		put(key, val);
+		return val;
     }
     
     void put(int key, int value) {
-        auto found = hmap.find(key);
-		if (found != hmap.end()) nodes.erase((*found).second);
-		else if (nodes.size() >= cap) {
-			int tbd = nodes.back().first;
-			nodes.pop_back();
-			hmap.erase(tbd);
-		} 
-		nodes.push_front({key, value});
-		hmap[key] = nodes.begin();
+        auto found = dict.find(key);
+		if (found != dict.end()) ls.erase((*found).second);
+		ls.push_front({key, value});
+		if (found != dict.end()) (*found).second = ls.begin();
+        else dict[key] = ls.begin();
+		if (ls.size() > cap) {
+            int key = ls.back().first;
+            ls.pop_back();
+            dict.erase(key);
+        }
     }
 };
 
