a6eef18da8d72b6b401d05926aafe9d3a6d440c5
Patrick Wu <magetron1@gmail.com>
2019-10-15 18:36:48 +0100

    refactor 259

---
+++ Leetcode/260.cc
@@ -0,0 +1,18 @@
+#include<bit/stdc++.h>
+#pragma GCC optimize ("Ofast")
+
+using namespace std;
+
+static int fast_io = [] () {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+	return 0;
+} ();
+
+class Solution {
+public:
+    vector<int> singleNumber(vector<int>& nums) {
+		
+    }
+};
