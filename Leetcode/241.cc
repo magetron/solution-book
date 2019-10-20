c56be5523f549ca29581623388ce60ab75560fc4
Patrick Wu <magetron1@gmail.com>
2019-10-20 18:56:10 +0100

    refactor 241

--- Leetcode/241.cc
+++ Leetcode/241.cc
@@ -1,6 +1,6 @@
 #include<bits/stdc++.h>
 #pragma GCC optimize ("Ofast")
-
+#define ll long long
 using namespace std;
 
 static int fast_io = [] () {
@@ -10,44 +10,58 @@ static int fast_io = [] () {
 	return 0;
 } ();
 
+struct hash_pair {
+    template <class T1, class T2>
+    size_t operator()(const pair<T1, T2>& p) const
+    {
+        auto hash1 = hash<T1>{}(p.first);
+        auto hash2 = hash<T2>{}(p.second);
+        return hash1 ^ hash2;
+    }
+};
+
 class Solution {
 public:
-	map<pair<int, int>, vector<int>> dict;
-
-	vector<int> eval (string& s, int l, int r) {
-		if (!dict[{l, r}].empty()) return dict[{l, r}];
+	vector<int> eval(int lp, int rp, string& s, unordered_map<pair<int, int>, vector<int>, hash_pair>& dict) {
+		if (dict.find({lp, rp}) != dict.end()) return dict[{lp, rp}];
+		bool found = false;
 		vector<int> ans;
-		for (int i = l; i <= r; i++) {
+		for (int i = lp; i <= rp; i++) {
 			if (!isdigit(s[i])) {
-				vector<int> l_sum = eval(s, l, i - 1);
-				vector<int> r_sum = eval(s, i + 1, r);
-				for (int ls : l_sum)
-					for (int rs : r_sum)
+				found = true;
+				vector<int> l = eval(lp, i - 1, s, dict);
+				vector<int> r = eval(i + 1, rp, s, dict);
+				for (int li : l) 
+					for (int ri : r) {
 						switch (s[i]) {
 							case '+':
-								ans.push_back(ls + rs);
+								ans.push_back(li + ri);
 								break;
 							case '-':
-								ans.push_back(ls - rs);
+								ans.push_back(li - ri);
 								break;
 							case '*':
-								ans.push_back(ls * rs);
+								ans.push_back(li * ri);
 								break;
 						}
+					}
 			}
 		}
-		if (ans.empty()) {
-			dict[{l, r}] = {stoi(s.substr(l, r - l + 1))};
-			return dict[{l, r}];
-		} else {
-			dict[{l, r}] = ans;
+		if (!found) {
+			ans.push_back(stoi(s.substr(lp, rp - lp + 1)));
+			dict[{lp, rp}] = ans;
+			return ans;
+		}
+		else {
+			dict[{lp, rp}] = ans;
 			return ans;
 		}
 	}
 
-    vector<int> diffWaysToCompute(string s) {
-		return eval(s, 0, s.length() - 1);
-	}
+    vector<int> diffWaysToCompute(string input) {
+		unordered_map<pair<int, int>, vector<int>, hash_pair> dict;
+		return eval(0, input.length() - 1, input, dict);
+    }
 };
 
 int main () {
