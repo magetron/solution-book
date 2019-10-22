f55d8c7899df0d6d2805d9d88969ae2886987901
Patrick Wu <magetron1@gmail.com>
2019-10-22 22:50:47 +0100

    solved 292

---
+++ Leetcode/292.cc
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+#pragma GCC optimize ("Ofast")
+
+using namespace std;
+
+static int fast_io = [] () {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+	return 0;
+} ();
+
+class Solution {
+public:
+    bool canWinNim(int n) {
+		return n % 4 != 0; 
+    }
+};
