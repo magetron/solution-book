5a640fb419a4ddb398276b747686ecf69a2252e5
Patrick Wu <magetron1@gmail.com>
2019-10-30 14:37:56 +0000

    refactor 282

--- Leetcode/282.cc
+++ Leetcode/282.cc
@@ -1,56 +1,55 @@
 #include<bits/stdc++.h>
 #pragma GCC optimize ("Ofast")
-
 #define ll long long
 
 using namespace std;
 
 static int fast_io = [] () {
-	ios_base::sync_with_stdio(false);
-	cin.tie(nullptr);
-	cout.tie(nullptr);
-	return 0;
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 0;
 } ();
 
 class Solution {
 public:
-
-	void dfs (int pos, string& p, string& num, int& target, ll cur, ll prev, vector<string>& ans) {
-		if (pos == num.length()) {
-			//if (eval(path) == (ll)target) ans.push_back(path);
-			if ((ll)target == cur) ans.push_back(p);
-			return;
-		}
-		for (int i = pos; i < num.length(); i++) {
-			if (num[pos] == '0' && i > pos) break;
-			string s = num.substr(pos, i - pos + 1);
-			ll val = stol(s);
-			if (pos == 0) {
-				p += s;
-				dfs(i + 1, p, num, target, val, val, ans);
-				p.erase(p.end() - s.length(), p.end());
-			} else {
-				p += "+" + s;
-				dfs(i + 1, p, num, target, cur + val, val, ans);
-				p.erase(p.end() - s.length() - 1, p.end());
-				p += "-" + s;
-				dfs(i + 1, p, num, target, cur - val, -val, ans);
-				p.erase(p.end() - s.length() - 1, p.end());
-				p += "*" + s;
-				dfs(i + 1, p, num, target, cur - prev + prev * val, prev * val, ans);
-				p.erase(p.end() - s.length() - 1, p.end());
-			}
-		}		
-	}		
-
+    
+    void dfs (string& num, int pos, string& path, ll cur, ll prev, int& target, vector<string>& ans) {
+        if (pos == num.length()) {
+            if (cur == target) {
+                ans.push_back(path);
+            }
+            return;
+        }
+        for (int i = pos; i < num.length(); i++) {
+            if (pos == 0) {
+                string ns = num.substr(pos, i - pos + 1);
+                ll val = stol(ns);
+                path += ns;
+                dfs(num, i + 1, path, val, val, target, ans);
+                path.erase(path.length() - ns.length(), ns.length());
+            } else {
+                string ns = num.substr(pos, i - pos + 1);
+                ll val = stol(ns);
+                path += "+" + ns;
+                dfs(num, i + 1, path, cur + val, val, target, ans);
+                path.erase(path.length() - ns.length() - 1, ns.length() + 1);
+                path += "-" + ns;
+                dfs(num, i + 1, path, cur - val, -val, target, ans);
+                path.erase(path.length() - ns.length() - 1, ns.length() + 1);
+                path += "*" + ns;
+                dfs(num, i + 1, path, prev * val + cur - prev, prev * val, target, ans);
+                path.erase(path.length() - ns.length() - 1, ns.length() + 1);
+            }
+            if (num[pos] == '0') break;
+        }
+    }
+    
     vector<string> addOperators(string num, int target) {
-		if (!num.length()) return {};
-		vector<string> ans;	
-		string path;
-		dfs(0, path, num, target, 0, 0, ans);
-		//path.clear();
-		//path.push_back('-'); path.push_back(num[0]);
-		//dfs(1, path, num, target, ans);
-		return ans;
-	}
+        if (!num.length()) return {};
+        string path{};
+        vector<string> ans;
+        dfs(num, 0, path, 0, 0, target, ans);
+        return ans;
+    }
 };
