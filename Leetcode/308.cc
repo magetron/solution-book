27fa23a0202b66e9fb92b7e7dfa6d544d3928b3f
Patrick Wu <magetron1@gmail.com>
2019-11-15 20:08:14 +0000

    solved 308

--- Leetcode/308.cc
+++ Leetcode/308.cc
@@ -10,63 +10,42 @@ static int fast_io = [] () {
 	return 0;
 } ();
 
-class NumArray {
-public:
-	vector<int> bit;
-	vector<int> n;
-
-    NumArray(vector<int>& nums) {
-		n = vector<int>(nums.size(), 0);
-		bit = vector<int>(nums.size() + 1);
-		for (int i = 0; i < nums.size(); i++) update(i, nums[i]);
-    }
-
-    void update(int i, int val) {
-		int cpy = val;
-        val -= n[i];
-        n[i] = cpy;
-		i++;
-		while (i < bit.size()) {
-			bit[i] += val;
-			i += i & (-i);
-		}
-    }
-
-	inline int sum (int i) {
-		i++;
-		int s = 0;
-		while (i > 0) {
-			s += bit[i];
-			i -= i & (-i);
-		}
-		return s;
-	}
-
-    int sumRange(int i, int j) {
-		return sum(j) - sum(i - 1);
-    }
-};
-
 class NumMatrix {
 public:
-	vector<NumArray> mat;
+	vector<vector<int>> mat;
+	vector<vector<int>> bit;
 
     NumMatrix(vector<vector<int>>& matrix) {
-		for (vector<int> line : matrix) {
-			mat.push_back(NumArray(line));
-		}
+		int m = matrix.size();
+		if (!m) return;
+		int n = matrix[0].size();
+		if (!n) return;
+		bit = vector<vector<int>>(m + 1, vector<int>(n + 1, 0));
+		mat = vector<vector<int>>(m, vector<int>(n, 0));
+		for (int i = 0; i < m; i++)
+			for (int j = 0; j < n; j++) update(i, j, matrix[i][j]);
     }
 
     void update(int row, int col, int val) {
-		mat[row].update(col, val);
+		int diff = val - mat[row][col];
+		mat[row][col] = val;
+		row++;
+		col++;
+		for (int i = row; i < bit.size(); i += (i & (-i))) 
+			for (int j = col; j < bit[0].size(); j += (j & (-j))) bit[i][j] += diff;	
     }
 
+	inline int sum (int row, int col) {
+		int s = 0;
+		row++;
+		col++;
+		for (int i = row; i > 0; i -= (i & (-i)))
+			for (int j = col; j > 0; j -= (j & (-j))) s += bit[i][j];
+		return s;
+	}
+
     int sumRegion(int row1, int col1, int row2, int col2) {
-		int sum = 0;
-		for (int i = row1; i <= row2; i++) {
-			sum += mat[i].sumRange(col1, col2);
-		}
-		return sum;
+		return sum(row2, col2) - sum(row2, col1 - 1) - sum(row1 - 1, col2) + sum(row1 - 1, col1 - 1);
     }
 };
 
@@ -76,4 +55,3 @@ public:
  * obj->update(row,col,val);
  * int param_2 = obj->sumRegion(row1,col1,row2,col2);
  */
-
