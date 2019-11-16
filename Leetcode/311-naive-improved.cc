7fd9eaf5def1c145678c93394e3266474cf0a49f
Patrick Wu <magetron1@gmail.com>
2019-11-16 23:03:02 +0000

    solved 311 naively with improved optimisation

---
+++ Leetcode/311-naive-improved.cc
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+#pragma GCC optimize ("Ofast")
+
+using namespace std;
+
+static int fast_io = [] () {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+	return 0;
+} ();
+
+class Solution {
+public:
+    vector<vector<int>> multiply(vector<vector<int>>& A, vector<vector<int>>& B) {
+		int mA = A.size();
+		if (!mA) return {{}};
+		int nA = A[0].size();
+		if (!nA) return {{}};
+		int mB = B.size();
+		if (!mB) return {{}};
+		int nB = B[0].size();
+		if (!nB) return {{}};
+		vector<vector<int>> ans(mA, vector<int>(nB, 0));
+		for (int i = 0; i < mA; i++) 
+			for (int k = 0; k < nA; k++) if (A[i][k] != 0)
+				for (int j = 0; j < nB; j++) if (B[k][j] != 0) 
+					ans[i][j] += A[i][k] * B[k][j];
+		return ans;
+    }
+};
+
