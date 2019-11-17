d98604bf1968c47809fdfdbc4e4080adaf0939d1
Patrick Wu <magetron1@gmail.com>
2019-11-17 18:53:43 +0000

    add class templates

---
+++ HackerRank/C++/class-templates.cc
@@ -0,0 +1,66 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <cassert>
+using namespace std;
+#pragma GCC optimize ("Ofast")
+
+static int fast_io = [] () {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 0;
+} ();
+
+template <class T>
+class AddElements {
+    T element;
+    public:
+    AddElements (T arg) : element(arg) { }
+
+    T add (T arg) {
+        return element + arg;
+    }
+};
+
+template <>
+class AddElements <string> {
+    string element;
+    public:
+    AddElements (string arg) : element(arg) { }
+
+    string concatenate (string& arg) {
+        return element + arg;
+    }
+};
+
+int main () {
+  int n,i;
+  cin >> n;
+  for(i=0;i<n;i++) {
+    string type;
+    cin >> type;
+    if(type=="float") {
+        double element1,element2;
+        cin >> element1 >> element2;
+        AddElements<double> myfloat (element1);
+        cout << myfloat.add(element2) << endl;
+    }
+    else if(type == "int") {
+        int element1, element2;
+        cin >> element1 >> element2;
+        AddElements<int> myint (element1);
+        cout << myint.add(element2) << endl;
+    }
+    else if(type == "string") {
+        string element1, element2;
+        cin >> element1 >> element2;
+        AddElements<string> mystring (element1);
+        cout << mystring.concatenate(element2) << endl;
+    }
+  }
+  return 0;
+}
+
