d453382ebbd06cf3076f52d42fc03e618e80ae98
Patrick Wu <magetron1@gmail.com>
2020-04-07 23:02:26 +0100

    update 321 WA

--- Leetcode/321.cc
+++ Leetcode/321.cc
@@ -25,7 +25,25 @@ public:
 	}
 
     vector<int> maxNumber(vector<int>& nums1, vector<int>& nums2, int k) {
-					
+		vector<int> n1(nums1);
+		vector<int> n2(nums2);
+		sort(n1.begin(), n1.end(), greater<int>());
+		sort(n2.begin(), n2.end(), greater<int>());
+		// optimise to sort top k elements;
+		for (int i = 0; i <= k && i <= nums1.size() && k - i <= nums2.size(); i++) {
+			int j = k - i;
+			int n1_bar = i > 0 ? n1[i - 1] : 10;
+			int n2_bar = j > 0 ? n2[j - 1] : 10;
+			cout << n1_bar << " " << n2_bar << endl;
+			vector<int> p1; p1.reserve(i);
+			vector<int> p2; p2.reserve(j);
+			for (int n : nums1) if (n >= n1_bar) p1.push_back(n);
+			for (int n : nums2) if (n >= n2_bar) p2.push_back(n);
+			for (auto p : p1) cout << p << " "; cout << endl;
+			for (auto p : p2) cout << p << " "; cout << endl;
+			cout << endl;
+		}
+		return {};
     }
 };
 
