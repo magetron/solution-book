cc772b1b773c1794d1440521f495441b54e72c6e
Patrick Wu <magetron1@gmail.com>
2020-04-09 17:11:24 +0100

    solved 321

--- Leetcode/321.cc
+++ Leetcode/321.cc
@@ -1,6 +1,8 @@
 #include<bits/stdc++.h>
 #pragma GCC optimize ("Ofast")
 
+#define si short int
+
 using namespace std;
 
 static int fast_io = [] () {
@@ -12,7 +14,12 @@ static int fast_io = [] () {
 
 class Solution {
 public:
-	bool cmp (vector<int>& a, vector<int>& b) {
+
+	inline bool max (int a, int b) {
+		return a > b;
+	}
+
+	inline bool cmp (vector<int>& a, vector<int>& b) {
 		if (a.size() == b.size()) {
 			int i = 0; 
 			while (i < a.size()) {
@@ -24,34 +31,78 @@ public:
 		} else return a.size() > b.size();
 	}
 
-    vector<int> maxNumber(vector<int>& nums1, vector<int>& nums2, int k) {
-		vector<int> n1(nums1);
-		vector<int> n2(nums2);
-		sort(n1.begin(), n1.end(), greater<int>());
-		sort(n2.begin(), n2.end(), greater<int>());
-		// optimise to sort top k elements;
-		for (int i = 0; i <= k && i <= nums1.size() && k - i <= nums2.size(); i++) {
-			int j = k - i;
-			int n1_bar = i > 0 ? n1[i - 1] : 10;
-			int n2_bar = j > 0 ? n2[j - 1] : 10;
-			cout << n1_bar << " " << n2_bar << endl;
-			vector<int> p1; p1.reserve(i);
-			vector<int> p2; p2.reserve(j);
-			for (int n : nums1) if (n >= n1_bar) p1.push_back(n);
-			for (int n : nums2) if (n >= n2_bar) p2.push_back(n);
-			for (auto p : p1) cout << p << " "; cout << endl;
-			for (auto p : p2) cout << p << " "; cout << endl;
-			cout << endl;
+
+	inline vector<int> max_num(vector<int>& ns, int k) {
+		vector<int> ans; ans.reserve(k);
+		for (int i = 0; i < ns.size(); i++) {
+			while (ns.size() - i + ans.size() > k && ans.size() > 0 && ns[i] > ans.back()) ans.pop_back();
+			if (ans.size() < k) ans.emplace_back(ns[i]);
 		}
-		return {};
+		return ans;
+	}
+
+
+	inline vector<int> merge(vector<int>& ns1, vector<int>& ns2, int k) {
+		vector<int> ans; ans.reserve(k);
+		int i = 0, j = 0;
+		bool checked = false, result = false;
+		int it = 0;
+		while (it < k && i < ns1.size() && j < ns2.size()) {
+			if (ns1[i] > ns2[j]) {
+				checked = false;
+				ans.emplace_back(ns1[i]);
+				i++; it++;
+			} else if (ns1[i] < ns2[j]) {
+				checked = false;
+				ans.emplace_back(ns2[j]);
+				j++; it++;
+			} else {
+				if (checked) {
+					if (result) {
+						ans.emplace_back(ns1[i]);
+						i++; it++;
+					} else {
+						ans.emplace_back(ns2[j]);
+						j++; it++;
+					}
+				} else {
+					checked = true;
+					int li = i + 1, lj = j + 1;
+					while (li < ns1.size() && lj < ns2.size() && ns1[li] == ns2[lj]) { li++; lj++; }
+					result = lj == ns2.size() || (li < ns1.size() && ns1[li] > ns2[lj]);
+				}
+			}
+		}
+		while (i < ns1.size()) {
+			ans.emplace_back(ns1[i]); i++;
+		}
+		while (j < ns2.size()) {
+			ans.emplace_back(ns2[j]); j++;
+		}
+		return ans;
+	}
+
+    vector<int> maxNumber(vector<int>& nums1, vector<int>& nums2, int k) {
+		vector<int> ans; ans.reserve(k);
+		for (int i = max(0, k - nums2.size()); i <= k && i <= nums1.size(); i++) {
+			vector<int> t1 = max_num(nums1, i);
+			vector<int> t2 = max_num(nums2, k - i);
+			vector<int> sol = merge(t1, t2, k);
+			if (cmp(sol, ans)) ans = sol;
+		}
+		return ans;
     }
 };
 
 int main () {
+	//vector<int> n1{6, 7};
+	//vector<int> n2{6, 0, 4};
 	vector<int> n1{3, 4, 6, 5};
 	vector<int> n2{9, 1, 2, 5, 8, 3};
+	//vector<int> n1{3, 9};
+	//vector<int> n2{8, 9};
 	Solution s;
-	auto ans = s.maxNumber(n1, n2, 5);
+	auto ans = s.maxNumber(n1, n2, 3);
 	for (auto a : ans) cout << a << " "; cout << endl;
 	return 0;
 }
